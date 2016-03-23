31e12d4ccc0f3dfe3a567578e6b3215c86f05863
Alexandre Strube raspberry pi 1 <surak@surak.eti.br>
2016-03-23 01:49:12 +0100

    this fswebcam sucks. It produces a lot of bad pictures

--- webcam/timelapse.sh
+++ webcam/timelapse.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # This script depends on "fswebcam", in order to capture pictures. 
+# To convert them into a video, the package is "libav-tools"
 # the cronjob would be something like
 # * * * * * ~/Devel/Raspberry/webcam/timelapse.sh 2>&1
+# avconv -f image2 -i foo-%03d.jpeg -r 12 -s WxH foo.avi
+# mencoder is out, the other tool is out. avconv is in
+
 
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-fswebcam -r 640x480 ~/Devel/Raspberry/webcam/images/$DATE.jpg
+fswebcam --fps 15 -S 8 -r 640x480 ~/Devel/Raspberry/webcam/images/$DATE.jpg
