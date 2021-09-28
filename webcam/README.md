b3551ba56fd32dbffbaef263e59f29dae8f8e0bd
surak <surak@surak.eti.br>
2021-09-28 19:36:24 +0200

    update on timelapse

--- webcam/README.md
+++ webcam/README.md
@@ -1,3 +1,12 @@
 # Time-lapse test
 
-This is just a time-lapse test
+This takes pictures every time specified in crontab. Edit with `crontab -e`
+
+For every minute, add this:
+
+```
+* * * * * ~/Devel/Raspberry/webcam/timelapse.sh 2>&1
+```
+
+In order to generate a timelapse, do this from the images directory:
+```ffmpeg -r 12 -pattern_type glob -i '*.jpg' -s hd1080 -vcodec libx264 timelapse.mp4```
--- webcam/timelapse.sh
+++ webcam/timelapse.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# This script depends on "fswebcam", in order to capture pictures. 
-# To convert them into a video, the package is "libav-tools"
-# the cronjob would be something like
+# This should be on your crontab
 # * * * * * ~/Devel/Raspberry/webcam/timelapse.sh 2>&1
-# avconv -f image2 -i foo-%03d.jpeg -r 12 -s WxH foo.avi
-# mencoder is out, the other tool is out. avconv is in
 
 # this should be in the /etc/modules for the pi noir infrared camera
 # besides the raspian-config camera module enabled
 ## Raspi noir video4linux
 #bcm2835-v4l2
 
+# To generate a timelapse: 
+# ffmpeg -r 12 -pattern_type glob -i '*.jpg' -s hd1080 -vcodec libx264 timelapse.mp4
 
 DATE=$(date +"%Y-%m-%d_%H%M")
+raspistill -awb auto -o $HOME/images/$DATE.jpg
 
-fswebcam --fps 15 -S 8 -r 640x480 ~/Devel/Raspberry/webcam/images/$DATE.jpg
+
